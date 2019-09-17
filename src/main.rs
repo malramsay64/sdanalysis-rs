850c905b1feab1fe2c1327fade667b71ef59c821
Malcolm Ramsay <malramsay64@gmail.com>
2019-09-17 14:31:49 +1000

    feat: Calculate and output orientational ordering
    This outputs the orientational ordering to a csv file, using Serde to
    serialise the values to csv.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,27 @@
 use clap::{App, Arg};
+use indicatif::ProgressIterator;
+use serde::Serialize;
 
+use csv;
 use gsd::GSDTrajectory;
+use sdanalysis::orientational_order;
+
+#[derive(Serialize)]
+struct Row {
+    molecule: usize,
+    timestep: usize,
+    orient_order: f64,
+}
+
+impl Row {
+    fn new(molecule: usize, timestep: usize, orient_order: f64) -> Self {
+        Row {
+            molecule,
+            timestep,
+            orient_order,
+        }
+    }
+}
 
 fn main() {
     let matches = App::new("GSD Parser")
@@ -12,13 +33,22 @@ fn main() {
                 .required(true)
                 .help("File to process"),
         )
+        .arg(
+            Arg::with_name("outfile")
+                .index(2)
+                .required(true)
+                .help("File to output csv data to"),
+        )
         .get_matches();
 
     let filename = matches.value_of("filename").unwrap();
 
-    for frame in GSDTrajectory::new(filename).take(4) {
-        println!("{}", frame.timestep);
-    }
+    let mut wtr = csv::Writer::from_path("test.csv").unwrap();
 
-    println!("filename: {}", filename)
+    for frame in GSDTrajectory::new(filename).progress() {
+        for (index, value) in orientational_order(&frame, 3.5).into_iter().enumerate() {
+            wtr.serialize(Row::new(index, frame.timestep as usize, value))
+                .unwrap();
+        }
+    }
 }
