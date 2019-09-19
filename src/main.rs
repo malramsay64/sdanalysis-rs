26e485e3073eb2771ac77455eec975f866da5c97
Malcolm Ramsay <malramsay64@gmail.com>
2019-09-20 08:26:52 +1000

    feat: Additional information in csv file
    This outputs a larger collection of information to the csv file making
    it more useful to make comparisons with the python implementation.

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,24 @@ use serde::Serialize;
 
 use csv;
 use gsd::GSDTrajectory;
-use sdanalysis::orientational_order;
+use itertools::izip;
+use sdanalysis::{num_neighbours, orientational_order};
 
 #[derive(Serialize)]
 struct Row {
     molecule: usize,
     timestep: usize,
     orient_order: f64,
+    num_neighbours: usize,
 }
 
 impl Row {
-    fn new(molecule: usize, timestep: usize, orient_order: f64) -> Self {
+    fn new(molecule: usize, timestep: usize, orient_order: f64, num_neighbours: usize) -> Self {
         Row {
             molecule,
             timestep,
             orient_order,
+            num_neighbours,
         }
     }
 }
@@ -45,10 +48,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let outfile = matches.value_of("outfile").unwrap();
 
     let mut wtr = csv::Writer::from_path(outfile)?;
+    let neighbour_distance = 3.5;
 
-    for frame in GSDTrajectory::new(filename)?.progress() {
-        for (index, value) in orientational_order(&frame, 3.5).into_iter().enumerate() {
-            wtr.serialize(Row::new(index, frame.timestep as usize, value))
+    for frame in GSDTrajectory::new(filename)?.take(100).progress() {
+        for (index, order, neighs) in izip!(
+            0..,
+            orientational_order(&frame, neighbour_distance),
+            num_neighbours(&frame, neighbour_distance),
+        ) {
+            wtr.serialize(Row::new(index, frame.timestep as usize, order, neighs))
                 .unwrap();
         }
     }
