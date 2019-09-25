e3d9240ab7b655b56898125787f2c66c24a9153b
Malcolm Ramsay <malramsay64@gmail.com>
2019-09-25 14:34:34 +1000

    fix: Move distance tests to frame module
    The tests that the periodic distance was working properly with the
    KDTree implementation are now in the frame module where the KD tree is
    defined.

--- src/order.rs
+++ src/order.rs
@@ -48,36 +48,4 @@ pub fn orientational_order(frame: &Frame, num_neighbours: usize) -> Vec<f64> {
 mod tests {
     use super::*;
 
-    #[test]
-    fn distance() {
-        let test_cell = [2., 2., 2., 0., 0., 0.];
-        let p = Position::new(&[0.; 3], 0, &test_cell);
-        let distance = p.distance_2(&[1., 0., 0.]);
-        assert_eq!(distance, 1.)
-    }
-
-    #[test]
-    fn distance_periodic() {
-        let test_cell = [2., 2., 2., 0., 0., 0.];
-        let p = Position::new(&[0.; 3], 0, &test_cell);
-        let distance = p.distance_2(&[2., 0., 0.]);
-        assert_eq!(distance, 0.)
-    }
-
-    #[test]
-    fn distance_within() {
-        let test_cell = [1., 1., 1., 0., 0., 0.];
-        let p = Position::new(&[0.; 3], 0, &test_cell);
-        assert_eq!(
-            p.distance_2_if_less_or_equal(&[0.5, 0., 0.], 0.5),
-            Some(0.25)
-        );
-    }
-
-    #[test]
-    fn distance_within_periodic() {
-        let test_cell = [1., 1., 1., 0., 0., 0.];
-        let p = Position::new(&[0.; 3], 0, &test_cell);
-        assert_eq!(p.distance_2_if_less_or_equal(&[1., 0., 0.], 0.5), Some(0.));
-    }
 }
