eb1cab90eecae8d9ac196c6e7303c24d82d347b4
Malcolm Ramsay <malramsay64@gmail.com>
2019-09-26 18:34:16 +1000

    feat: Pass processing to secondary thread
    The reading of files is highly IO bound, so this puts the processing on
    a separate thread hopefully speeding everything up.

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,9 @@ use std::path::PathBuf;
 use failure::Error;
 use indicatif::ProgressIterator;
 use serde::Serialize;
+use std::sync::mpsc::channel;
 use structopt::StructOpt;
+use threadpool::ThreadPool;
 
 use csv;
 use gsd::GSDTrajectory;
@@ -22,16 +24,14 @@ struct Row {
     molecule: usize,
     timestep: usize,
     orient_order: f64,
-    num_neighbours: usize,
 }
 
 impl Row {
-    fn new(molecule: usize, timestep: usize, orient_order: f64, num_neighbours: usize) -> Self {
+    fn new(molecule: usize, timestep: usize, orient_order: f64) -> Self {
         Row {
             molecule,
             timestep,
             orient_order,
-            num_neighbours,
         }
     }
 }
@@ -57,6 +57,9 @@ fn main(args: Args) -> Result<(), Error> {
     let neighbour_distance = 8.;
     let nneighs = 6;
 
+    let n_workers = 1;
+    let pool = ThreadPool::new(n_workers);
+
     let trj = GSDTrajectory::new(&args.filename)?;
     let num_frames = match args.num_frames {
         Some(n) => n,
@@ -68,17 +71,20 @@ fn main(args: Args) -> Result<(), Error> {
             .template("{msg}{wide_bar} {per_sec} {pos}/{len} [{elapsed_precise}/{eta_precise}]"),
     );
 
-    for frame in trj
-        .map(Frame::from)
-        .take(num_frames)
-        .progress_with(progress_bar)
-    {
-        for (index, order, neighs) in izip!(
-            0..,
-            orientational_order(&frame, nneighs),
-            num_neighbours(&frame, neighbour_distance),
-        ) {
-            wtr.serialize(Row::new(index, frame.timestep as usize, order, neighs))?;
+    let (tx, rx) = channel();
+    for frame in trj.take(num_frames).progress_with(progress_bar) {
+        let tx = tx.clone();
+        pool.execute(move || {
+            tx.send((
+                frame.timestep,
+                orientational_order(&Frame::from(frame), nneighs),
+            ))
+            .expect("channel will be there waiting for the pool");
+        });
+    }
+    for (timestep, result) in rx {
+        for (index, order) in result.iter().enumerate() {
+            wtr.serialize(Row::new(index, timestep as usize, *order))?;
         }
     }
     wtr.flush().expect("Flushing file failed");
