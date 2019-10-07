53b6bd2611d2e38ee03903a356afa4bb556cdc28
Malcolm Ramsay <malramsay64@gmail.com>
2019-10-07 17:48:39 +1100

    refactor: Use my fork of rust_voronoi
    This uses a purely rust implementation of creating the voronoi diagrams,
    which results in a 10 fold performance increase for the voronoi
    diagrams.

--- src/voronoi.rs
+++ src/voronoi.rs
@@ -6,10 +6,8 @@
 
 use crate::distance::make_cartesian;
 use crate::frame::Frame;
-use failure::{bail, err_msg, Error};
-use geo::prelude::Area;
-use geo_types::{LineString, Point, Polygon};
-use geos::from_geo::TryInto;
+use failure::Error;
+use voronoi::{make_polygons, voronoi, Cell, Point};
 
 /// Compute the voronoi area for each particle in a frame
 ///
@@ -17,27 +15,48 @@ use geos::from_geo::TryInto;
 /// within a Frame. Currently this doesn't take into account of the periodic boundary conditions.
 ///
 pub fn voronoi_area(frame: &Frame) -> Result<Vec<f64>, Error> {
-    let points: Vec<Point<f64>> = frame
+    let points: Vec<Point> = frame
         .position
         .iter()
         .map(|p| Point::new(p[0] as f64, p[1] as f64))
         .collect();
 
-    let cell_corners: Vec<(f64, f64)> =
-        [[0., 0., 0.5], [1., 0., 0.5], [1., 1., 0.5], [0., 1., 0.5]]
-            .into_iter()
-            .map(|p| make_cartesian(&frame.simulation_cell, p))
-            .map(|p| (p[0] as f64, p[1] as f64))
-            .collect();
+    let cell_corners: Vec<_> = [[0., 0., 0.5], [1., 0., 0.5], [1., 1., 0.5], [0., 1., 0.5]]
+        .into_iter()
+        .map(|p| make_cartesian(&frame.simulation_cell, p))
+        .map(|p| Point::new(p[0] as f64, p[1] as f64))
+        .collect();
 
-    let geo_poly = Polygon::new(LineString::from(cell_corners), vec![]);
-    let boundary: geos::Geometry = match geo_poly.try_into() {
-        Ok(g) => g,
-        Err(_) => bail!("Unable to create geometry"),
-    };
+    let boundary: Cell = Cell::from([
+        cell_corners[0],
+        cell_corners[1],
+        cell_corners[2],
+        cell_corners[3],
+    ]);
 
-    let polygons: Vec<_> = geos::compute_voronoi(&points, Some(&boundary), 0., false)
-        .map_err(|e| err_msg(e.to_string()))?;
+    let polygons: Vec<_> = make_polygons(&voronoi(points, &boundary));
 
-    Ok(polygons.into_iter().map(|p| p.area().abs()).collect())
+    Ok(polygons.into_iter().map(shoelace).collect())
+}
+
+fn shoelace(polygon: Vec<Point>) -> f64 {
+    polygon
+        .iter()
+        .zip(polygon.iter().cycle().skip(1))
+        .map(|(curr, next)| (next.x() + curr.x()) * (next.y() - curr.y()))
+        .sum::<f64>()
+        .abs()
+        / 2.
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use voronoi::Point;
+
+    #[test]
+    fn simple_area() {
+        let points = vec![Point::new(0., 1.), Point::new(2., 3.), Point::new(4., 7.)];
+        assert_eq!(shoelace(points), 2.)
+    }
 }
